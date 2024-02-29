fec197cbdb1858db3282d7eac60a95d83170385a
rRedq <redq_000@mail.ru>
2024-02-29 22:17:01 +0500

    feat: add solutions for some tasks

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -33,8 +33,15 @@ function shallowCopy(obj) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  const newObj = {};
+  objects.forEach((element) => {
+    Object.entries(element).forEach(([key, value]) => {
+      if (newObj[key]) newObj[key] += value;
+      else newObj[key] = value;
+    });
+  });
+  return newObj;
 }
 
 /**
@@ -106,8 +113,8 @@ function isEmptyObject(obj) {
  *    immutableObj.newProp = 'new';
  *    console.log(immutableObj) => {a: 1, b: 2}
  */
-function makeImmutable(/* obj */) {
-  throw new Error('Not implemented');
+function makeImmutable(obj) {
+  return Object.freeze(obj);
 }
 
 /**
@@ -120,8 +127,14 @@ function makeImmutable(/* obj */) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  const arr = [];
+  Object.entries(lettersObject).forEach(([key, value]) => {
+    value.forEach((item) => {
+      arr[item] = key;
+    });
+  });
+  return arr.join('');
 }
 
 /**
@@ -138,8 +151,12 @@ function makeWord(/* lettersObject */) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(/* queue */) {
-  throw new Error('Not implemented');
+function sellTickets(queue) {
+  let sum = 0;
+  queue.forEach((element) => {
+    sum += element;
+  });
+  return sum <= 100;
 }
 
 /**
@@ -155,8 +172,20 @@ function sellTickets(/* queue */) {
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  class Obj {
+    constructor(a, b) {
+      this.width = a;
+      this.height = b;
+    }
+
+    getArea() {
+      return this.width * this.height;
+    }
+  }
+  const newObj = new Obj(width, height);
+
+  return newObj;
 }
 
 /**
